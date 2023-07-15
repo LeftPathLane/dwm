260f1e7ff344335b754d8c927f5a25ae5638ca56
LeftPathLane <paulsexecutables@gmail.com>
2023-07-15 10:32:11 +1000

    status2d

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int gappx     = 16;
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hasklug Nerd Font:size=12" };
-static const char dmenufont[]       = "Hasklug Nerd Font:size=12";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=16" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=16";
 static const char dmenulines[]      = "10";
 static char normbgcolor[]           = "#2E3440";
 static char normbordercolor[]       = "#3B4252";
@@ -16,6 +16,40 @@ static char normfgcolor[]           = "#D8DEE9";
 static char selfgcolor[]            = "#ECEFF4";
 static char selbordercolor[]        = "#A3BE8C";
 static char selbgcolor[]            = "#5E81AC";
+static char termcol0[] = "#000000"; /* black   */
+static char termcol1[] = "#ff0000"; /* red     */
+static char termcol2[] = "#33ff00"; /* green   */
+static char termcol3[] = "#ff0099"; /* yellow  */
+static char termcol4[] = "#0066ff"; /* blue    */
+static char termcol5[] = "#cc00ff"; /* magenta */
+static char termcol6[] = "#00ffff"; /* cyan    */
+static char termcol7[] = "#d0d0d0"; /* white   */
+static char termcol8[]  = "#808080"; /* black   */
+static char termcol9[]  = "#ff0000"; /* red     */
+static char termcol10[] = "#33ff00"; /* green   */
+static char termcol11[] = "#ff0099"; /* yellow  */
+static char termcol12[] = "#0066ff"; /* blue    */
+static char termcol13[] = "#cc00ff"; /* magenta */
+static char termcol14[] = "#00ffff"; /* cyan    */
+static char termcol15[] = "#ffffff"; /* white   */
+static char *termcolor[] = {
+  termcol0,
+  termcol1,
+  termcol2,
+  termcol3,
+  termcol4,
+  termcol5,
+  termcol6,
+  termcol7,
+  termcol8,
+  termcol9,
+  termcol10,
+  termcol11,
+  termcol12,
+  termcol13,
+  termcol14,
+  termcol15,
+};
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
@@ -31,7 +65,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating  isterminal  noswallow  monitor */
-  {"st",        NULL,       NULL,       0,            0,          1,          0,         -1 },
+  {"St",        NULL,       NULL,       0,            0,          1,          0,         -1 },
   {"st-scratch",NULL,       NULL,       0,            1,          1,          1,         -1 },
 };
 

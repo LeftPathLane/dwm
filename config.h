a2820d0f8436839f603a2428397b90718be81ae2
LeftPathLane <paulsexecutables@gmail.com>
2024-04-29 20:48:41 +1000

    Extra keybinds

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx     = 16;
+static const unsigned int snap      = 16;       /* snap pixel */
+static const unsigned int gappx     = 0;/*16*/
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -67,10 +67,12 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating  isterminal  noswallow  monitor */
   {"St",        NULL,       NULL,       0,            0,          1,          0,         -1 },
   {"st-scratch",NULL,       NULL,       0,            1,          1,          1,         -1 },
+  {"discord",   NULL,       NULL,       1 << 3,       0,          0,          1,         -1 },
+  {"xev", NULL, NULL, 0, 0, 0, 1, -1},
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -108,6 +110,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Print,  spawn,          SHCMD("scrot -F - | xclip -sel clip -t image/png") },
 	{ MODKEY|ShiftMask,             XK_Print,  spawn,          SHCMD("scrot -s -F - | xclip -sel clip -t image/png") },
 	{ MODKEY,                       XK_apostrophe, spawn,      SHCMD("st -c st-scratch") },
+  { MODKEY|ShiftMask,             XK_semicolon,  spawn,      SHCMD("tabbed -r 2 surf -e ID -gN https://google.com") },
+  { MODKEY,                       XK_semicolon,  spawn,      SHCMD("surf -gN https://google.com") },
+  { MODKEY|ControlMask,           XK_semicolon,  spawn,      SHCMD("surf -gN `dmenu -l 10 < ~/.urls`") },
+	{ MODKEY,             XK_question, spawn,  SHCMD("st -c st-scratch") },//       SHCMD("tabbed -r 2 surf -e ID -gN https://google.com") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
